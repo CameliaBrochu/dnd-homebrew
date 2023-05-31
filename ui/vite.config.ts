ecad8342490c847bc6243375840102372b659398
Camélia Brochu <cameliabrochu@gmail.com>
2023-05-31 10:49:57 -0400

    add alias

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -12,8 +12,6 @@ export default defineConfig({
         }),
     ],
     resolve: {
-        alias: {
-          src: path.resolve(__dirname, 'src'),
-        }
-      },
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
 })
